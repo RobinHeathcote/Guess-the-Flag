c803b65e81a1da0414770a28ad4aedb78260dac7
Robin Heathcote <robin.heathcote@thebeansgroup.com>
2021-04-16 09:18:25 +0100

    Initial Commit

---
+++ Project 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Project 2
+//
+//  Created by Robin Heathcote on 16/04/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
